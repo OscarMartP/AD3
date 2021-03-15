ef14cdd70a8ffd1c1e30c225dcbfd20b2c711358
OscarMartP <71500380+OscarMartP@users.noreply.github.com>
2021-03-15 04:38:06 +0100

    Add files via upload

---
+++ AD3OscarLunes/src/main/java/models/Asignatura.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Asignatura {
+
+	private int cod_asignatura;
+	private String nombre;
+
+	public Asignatura(int cod_asignatura, String nombre) {
+		this.cod_asignatura = cod_asignatura;
+		this.nombre = nombre;
+	}
+
+	public int getCod_asignatura() {
+		return cod_asignatura;
+	}
+
+	public void setCod_asignatura(int cod_asignatura) {
+		this.cod_asignatura = cod_asignatura;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Asignatura [cod_asignatura=" + cod_asignatura + ", nombre=" + nombre + "]";
+	}
+}
